e64f3bea7a1d4fd8f25de0aaa4f4895c4f4c655e
Shahab-Athar <shahabathar1039@gmail.com>
2021-03-24 21:22:45 +0500

    Add api support And leaving project for learning for now

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Paper, Typography, TextField, Button } from '@material-ui/core';
 
 import useStyle from './styles'
 
 const Form = () => {
     const classes = useStyle();
+    const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <div>
-            <h1>Form</h1>
-        </div>
+        <Paper className={classes.paper}>
+            <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+                <Typography variant="h6">Creating A Memory</Typography>
+                <TextField
+                    name="creator"
+                    veriant="outlined"
+                    label="Creator"
+                    fullWidth
+                    value={postData.creator}
+                    onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
+                    />
+            </form>
+        </Paper>
     )
 }
 
